d0bed3495e80b229130c3348329dfcf29d8a0dd9
Sam Burton <sam.burton271@gmail.com>
2025-01-27 13:03:04 -0500

    Added README for the whole project, added steps for iptables setup

---
+++ README.txt
@@ -0,0 +1,9 @@
+This is a collection of custom security tools, primarily designed for use on linux machines in the CCDC competition.
+
+Port requirements:
+
+TCP/1973 - Arbiter
+	Used for log transfers between machines
+	
+TCP/1893 - Manticore
+	Used for messaging and automated responses between machines.
--- firewall/install.sh
+++ firewall/install.sh
@@ -3,6 +3,7 @@ if [ $EUID -ne 0 ]; then
     echo "Must be run as root"
 	exit
 fi
+repo_root=$(git rev-parse --show-toplevel)
 yum install -y nftables
 apt install -y nftables
 if [[ -f /bin/nft || -f /sbin/nft ]]; then
@@ -14,13 +15,13 @@ if [[ -f /bin/nft || -f /sbin/nft ]]; then
 		numVersion=$(echo "$version" | bc)
 		if (( $(echo "$numVersion < 0.5" | bc -l) )); then
 			echo "This machine is unable to support nftables version 0.5 or higher. Installing firewall using iptables."
-			#IPTABLES FIREWALL INSTALL
+			bash $repo_root/scripts/linux/firewall/ipTablesFirewall/setup.sh
 			exit
 		fi
 	fi
 else
 	echo "This machine is unable to support nftables. Installing firewall using iptables."
-	#IPTABLES FIREWALL INSTALL
+	bash $repo_root/scripts/linux/firewall/ipTablesFirewall/setup.sh
 	exit
 fi
 yum install -y python3
@@ -34,18 +35,17 @@ if [[ -f /bin/python3 ]]; then
 	secondNum=$((${versionSplit[1]}))
 	if [[ $firstNum -lt 3 ]]; then
 		echo "This machine is unable to support python3. Installing firewall using iptables."
-		#IPTABLES FIREWALL INSTALL
+		bash $repo_root/scripts/linux/firewall/ipTablesFirewall/setup.sh
 		exit
 	elif [[ $secondNum -lt 8 ]]; then
 		echo "This machine is unable to support python 3.8. Installing firewall using iptables."
-		#IPTABLES FIREWALL INSTALL
+		bash $repo_root/scripts/linux/firewall/ipTablesFirewall/setup.sh
 		exit
 	fi
 else
 	echo "This machine is unable to support python3. Installing firewall using iptables."
-	#IPTABLES FIREWALL INSTALL
+	bash $repo_root/scripts/linux/firewall/ipTablesFirewall/setup.sh
 	exit
 fi
 echo "This machine passes all dependency checks. Installing firewaill using nftables."
-repo_root=$(git rev-parse --show-toplevel)
 bash $repo_root/scripts/linux/firewall/nfTablesFirewall/setup.sh
