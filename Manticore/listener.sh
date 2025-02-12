38b4dbfb9fe3a837a1c6ef49cc93ad573d6dee5b
Sam Burton <sam.burton271@gmail.com>
2025-02-12 12:30:24 -0500

    Added basic layout for listener

--- Manticore/listener.sh
+++ Manticore/listener.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+LISTEN_PORT=18965
+processCommand() # Usage: processCommand {command}
+{
+	IS_PREDEFINED_CMD=$false
+	input="$1"
+	
+	IFS="-" read -ra inputSplit <<< "$input"
+	case "${inputSplit[0]}" in
+	"B45")
+		#Blacklist IP found in "${inputSplit[1]}"
+	;;
+	*)
+	;;
+	esac
+}
+while true; do
+	recievedMsg="$(nc -l -p $LISTEN_PORT)"
+	processCommand "$recievedMsg"
+	if ! $IS_PREDEFINED_CMD; then
+		#Code to process playbook input
+	fi
+done
