7285864b5af06fb31352eece2d4b17e7c4f52981
Sam Burton <sam.burton271@gmail.com>
2025-02-13 17:29:32 -0500

    Added code to utilize the 'IS_PREDEFINED_CMD' flag

--- Manticore/listener.sh
+++ Manticore/listener.sh
@@ -8,11 +8,13 @@ processCommand() # Usage: processCommand {command}
 	IFS="-" read -ra inputSplit <<< "$input"
 	case "${inputSplit[0]}" in
 		"B45")
+			IS_PREDEFINED_CMD=$true
 			firewall -ba "${inputSplit[1]}"
 			current_time=$(date +"%H:%M:%S")
 			echo "[$current_time] - Accepted blacklist command for IP: ${inputSplit[1]}" >> /var/log/manticore.log
 		;;
 		*)
+			
 		;;
 	esac
 }
