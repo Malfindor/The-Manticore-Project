b696dec24031325c413f68eed59c39f3ccee6716
malfindor <sam.burton271@gmail.com>
2025-04-08 10:13:39 -0400

    Fixed issue of Gemini Firewatch falsely flagging changes in iptables.

--- Gemini/modules/firewatch/firewatch.sh
+++ Gemini/modules/firewatch/firewatch.sh
@@ -3,7 +3,7 @@ validRules=$(< /etc/gemini/iptablesrules.bak)
 echo "$validRules" > /tmp/iptables_valid
 echo "" > /tmp/iptables_ack
 while true; do
-    iptables -L -v -n > /tmp/iptables_current
+    iptables -L > /tmp/iptables_current
     if ! diff /tmp/iptables_valid /tmp/iptables_current > /dev/null; then
 		if ! diff /tmp/iptables_ack /tmp/iptables_current > /dev/null; then
 			current_time=$(date +"%H:%M:%S")
